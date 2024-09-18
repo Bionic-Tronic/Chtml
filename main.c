f200b4caf3bf7d5394e9d9e57ea25c7f7738de04
Bionic-Tronic <quevainacongithub@gmail.com>
2024-09-17 20:06:08 -0500

    add main.c

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include "include/chtml.h"
+
+int main (){
+	struct chtml html;
+	html.port = 8090;
+	html.url = "127.0.0.1";
+	html.errors_on = true;
+	prepare_server(&html);
+	Chtml(&html);
+	html.h1(EMPTY,"Esta es mi pagina");
+	html.show_html_console();
+	while(true){
+		html.send(&html);
+	}
+	return 0;
+}
 
